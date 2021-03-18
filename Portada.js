a9230c88d148b0b863546e0653583a535575f4bb
= <=>
2021-03-18 18:13:51 +0100

    Trabajo del 18/3

--- Portada.js
+++ Portada.js
@@ -1,9 +1,17 @@
-function go(){
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-if (document.form.password.value=='CONTRASEÑA' && document.form.login.value=='USUARIO'){ 
-        document.form.submit(); 
-    } 
-    else{ 
-         alert("Nombre de usuario y/o contraseña incorrectos."); 
-    } 
-} 
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
