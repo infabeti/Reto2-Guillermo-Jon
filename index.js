340920f090118824b19814d59cebf571fd4b081c
Jon Gonzalez-Bals <jon.gonzalezbals@elorrieta-errekamari.com>
2021-03-26 15:31:00 +0100

    ventanas e icono

--- index.js
+++ index.js
@@ -4,31 +4,50 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
+
     let filePath = path.join(
+
         __dirname,
       
-        req.url === "/" ? "Portada.html" : req.url
+        req.url === "/" ? "BarPresley.html" : req.url
+
     );
 
     let extName = path.extname(filePath);
     let contentType = 'text/html';
 
     switch (extName) {
+
         case '.css':
+
             contentType = 'text/css';
+
             break;
+
         case '.js':
+
             contentType = 'text/javascript';
+
             break;
+
         case '.json':
+
             contentType = 'application/json';
+
             break;
+
         case '.png':
+
             contentType = 'image/png';
+
             break;
+
         case '.jpg':
+
             contentType = 'image/jpg';
+
             break;
+
     }
 
     console.log(`File path: ${filePath}`);
@@ -38,12 +57,18 @@ const server = http.createServer((req, res) => {
 
     const readStream = fs.createReadStream(filePath);
     readStream.pipe(res);
+
 });
 
 server.listen(port, (err) => {
+
     if (err) {
+
         console.log(`Error: ${err}`)
+ 
     } else {
+
         console.log(`Server listening at port ${port}...`);
+   
     }
 });
